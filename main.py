c17f31602d5f88f058cbe56b98620e3daa0ec6d6
Dhruv Jimulia <dhruvjimulia@gmail.com>
2021-07-10 03:28:03 +0530

    Added vectorizer loading/saving feature

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import pickle
 
 print("Imports complete")
 
-####### Data Preprocessing #######
+####### Data Preprocessing Functions #######
 def is_not_br(token):
     return ((not ('<br' in token.text)) & (not ('/><br' in token.text)) & (not ('/>' in token.text)) & (token.text != 'br') & (not ('<' in token.text)))
 def tokenize(text):
@@ -31,32 +31,52 @@ def convert_y(sentiments):
         converted_list.append(1 if sentiment == 'positive' else 0)
     return np.array(converted_list)
 
+
+###### Loading Data ######
 imdb = pd.read_csv('../imdbDataset.csv')
 imdb = imdb.iloc[:1500]
 pd.set_option("display.max_colwidth", None)
 print(f"Length : {len(imdb)}")
 
-print("Data Cleaning Complete")
+print("Data Loading Complete")
 
 ##### Word -> Number Encoding ######
 ##### Bag Of Words
 ##### Hyperparams: max_features
 def bag_of_words(X, y):
-    print("Starting creation of Bag of Words Model")
-    bow_transformer = CountVectorizer(analyzer=tokenize, max_features=2000).fit(X)
-    X = bow_transformer.transform(X)
-    y = convert_y(y)
-    print("Bag of Words Model Created")
+    print("Starting Bag of Words Model")
+    with open("is_vectorizer_created", "r") as fin:
+        if ('False' in fin.read()):
+            print("Did not find created model. Creating new model...")
+            bow_transformer = CountVectorizer(analyzer=tokenize, max_features=2000).fit(X)
+            pickle.dump(bow_transformer, open("bow_transformer.pickle", "wb"))
+        else:
+            print("Model Found. Loading Model...")
+            bow_transformer = pickle.load(open("bow_transformer.pickle", "rb"))
+        X = bow_transformer.transform(X)
+        y = convert_y(y)
+        print("Bag of Words Model Completed")
+    with open("is_vectorizer_created", "w") as fout:
+        fout.write("True")
     return X, y
 
 ##### TF-IDF Vectorization
 ##### Hyperparams: max_features
 def tf_idf(X, y):
-    print("Starting creation of TF-IDF Model")
-    tfidf_transformer = TfidfVectorizer(analyzer=tokenize, max_features=2000).fit(X)
-    X = tfidf_transformer.transform(X)
-    y = convert_y(y)
-    print("TF-IDF Model Created")
+    print("Starting TF-IDF Model")
+    with open("is_vectorizer_created", "r") as fin:
+        if ('False' in fin.read()):
+            print("Did not find created model. Creating new model...")
+            tfidf_transformer = TfidfVectorizer(analyzer=tokenize, max_features=2000).fit(X)
+            pickle.dump(tfidf_transformer, open("tfidf_transformer.pickle", "wb"))
+        else:
+            print("Model Found. Loading Model...")
+            tfidf_transformer = pickle.load(open("tfidf_transformer.pickle", "rb"))            
+        X = tfidf_transformer.transform(X)
+        y = convert_y(y)
+        print("TF-IDF Model Created")
+    with open("is_vectorizer_created", "w") as fout:
+        fout.write("True")
     return X, y
 
 ##### Pre-trained Word Embeddings (Word2Vec)
@@ -91,6 +111,9 @@ def knn_classifier(X, y, number_neighbors):
 ##### Applying Models And Printing Accuracy #####
 X, y = tf_idf(imdb['review'], imdb['sentiment'])
 
+max_accuracy = -1
+max_accuracy_neighbors = 0
+
 for i in range(20, 100):
     print(f"Number of neighbors: {i}")
     y_test, y_pred = knn_classifier(X, y, i)
@@ -101,3 +124,9 @@ for i in range(20, 100):
 
     print("Accuracy: ", round(accuracy, 2), ", Precision: ", round(precision, 2), ", Recall ", round(recall, 2))
     print("\n")
+
+    if (accuracy > max_accuracy):
+        max_accuracy = accuracy
+        max_accuracy_neighbors = i
+
+print(f"Maximum Accuracy obtained was {max_accuracy} with {max_accuracy_neighbors}")
